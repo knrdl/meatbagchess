223ed23a674c75c79d995bf80558143e8ceb1eaf
knrdl <35548889+knrdl@users.noreply.github.com>
2025-07-15 19:46:03 +0200

    feat: store piece style selection

--- client/src/FabSetStyle.svelte
+++ client/src/FabSetStyle.svelte
@@ -1,7 +1,7 @@
 <script>
     import { PAWN, WHITE } from 'chess.js';
     import PieceImg from './lib/chesspieces/PieceImg.svelte';
-    import { piecesStyle } from './lib/chesspieces/index.svelte';
+    import { PIECES_STYLE_KEY, piecesStyle } from './lib/chesspieces/index.svelte';
 </script>
 
 <button
@@ -9,6 +9,7 @@
     style="position: fixed;right:1rem;bottom:1rem;border-radius:50%;padding:.25rem;width:2.5rem;height:2.5rem;background-color:#6663"
     onclick={() => {
         piecesStyle.selected = piecesStyle.selected === 'standard' ? 'cats' : 'standard';
+        localStorage.setItem(PIECES_STYLE_KEY, piecesStyle.selected);
     }}
 >
     <PieceImg style={piecesStyle.selected === 'standard' ? 'cats' : 'standard'} color={WHITE} type={PAWN} --size="1.5rem" />
--- client/src/lib/chesspieces/index.svelte.ts
+++ client/src/lib/chesspieces/index.svelte.ts
@@ -24,12 +24,14 @@ import wpCat from './cats/wP.png'
 import wqCat from './cats/wQ.png'
 import wrCat from './cats/wR.png'
 
+
 const piecesImages: Record<'standard' | 'cats', Record<`${Color}${PieceSymbol}`, string>> = {
     standard: { bb: bbStandard, bk: bkStandard, bn: bnStandard, bp: bpStandard, bq: bqStandard, br: brStandard, wb: wbStandard, wk: wkStandard, wn: wnStandard, wp: wpStandard, wq: wqStandard, wr: wrStandard },
     cats: { bb: bbCat, bk: bkCat, bn: bnCat, bp: bpCat, bq: bqCat, br: brCat, wb: wbCat, wk: wkCat, wn: wnCat, wp: wpCat, wq: wqCat, wr: wrCat },
 }
 
-export let piecesStyle = $state<{ selected: keyof typeof piecesImages }>({ selected: 'standard' })
+export const PIECES_STYLE_KEY = 'selectedPiecesStyle'
+export let piecesStyle = $state<{ selected: keyof typeof piecesImages }>({ selected: (localStorage.getItem(PIECES_STYLE_KEY) ?? 'standard') as keyof typeof piecesImages })
 
 import { type Color, type Piece, type PieceSymbol } from 'chess.js'
 
