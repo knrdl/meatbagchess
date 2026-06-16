673f7af59e1f58e509fd76173058e0387b3f19a7
knrdl <35548889+knrdl@users.noreply.github.com>
2026-06-16 09:18:26 +0000

    chore: upgrade deps

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,6 @@
 import { mount } from 'svelte'
 import './app.css'
-import '@fontsource/orbitron'
+import '@fontsource/orbitron/index.css'
 import App from './App.svelte'
 
 const app = mount(App, {
